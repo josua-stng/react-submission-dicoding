e4847e99e64dec76261fe52a8ec42ae4ec79973a
josua-stng <josuasitanggang51@gmail.com>
2024-02-07 08:46:36 +0700

    feat: add logout feature

--- src/app/lib/user-login.tsx
+++ src/app/lib/user-login.tsx
@@ -1,10 +1,24 @@
 'use client';
+
+import { useRouter } from 'next/navigation';
+
 export default function UserLogin({ user }: { user: string }) {
+  const router = useRouter();
+  const handlerUserLogout = () => {
+    localStorage.removeItem('token');
+    router.push('/auth/sign-in');
+  };
   return (
-    <div className="border border-black w-max m-2 px-5 py-3 rounded-md">
+    <div className="border border-black w-max m-2 px-5 py-3 rounded-md space-y-1">
       <p>
         hello <span className="font-bold">{user}</span>{' '}
       </p>
+      <button
+        className="bg-red-600 text-white px-3 py-0.5 rounded-md"
+        onClick={handlerUserLogout}
+      >
+        Logout
+      </button>
     </div>
   );
 }
